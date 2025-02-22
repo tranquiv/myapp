1fdf92b94cf08e795e5a7792bd71a900a7d51957
tranquiv <ocamposmarceloivan@gmail.com>
2025-02-22 18:27:33 -0300

    Añadido BrowserRouter con basename

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/myapp">  {/* Agrega el basename con el nombre de tu app */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
